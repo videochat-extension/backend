50983ccddd43bc466baa2ade7cddc55f1bf5cc1b
qrlk <hi@qrlk.me>
2023-04-26 06:34:13 +0700

    change allowed_hosts to production domain

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [ "api-videochat-extension.starbasewiki.com" ]
+ALLOWED_HOSTS = [ "api-videochat-extension.starbase.wiki" ]
 
 # Application definition
 
